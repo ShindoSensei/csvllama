8f79e8a46243a7b8a48fdfe68395b652ba97c962
Justin Teo <justin.tsw@gmail.com>
2017-05-29 00:12:54 +0800

    Front end form validation for homepage done

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,29 +7,31 @@ class MainController < ApplicationController
 
   def import_csv
     require 'csv'
-    File.foreach(params[:file].path, headers:true) do |csv_line|
-      row = CSV.parse(csv_line.gsub('\"', '""')).first #For RubyCSV's inability to recognise escapes
-      if row[1] == "Order"
-        Order.find_or_create_by(id:row[0]) #Creates new order if non-existent
-        OrderUpdate.create(
-          order_id: row[0],
-          time_stamp: row[2],
-          object_changes: JSON.parse(row[3])
-        )
-      elsif row[1] == "Invoice"
-        Invoice.find_or_create_by(id:row[0])
-        InvoiceUpdate.create(
-          invoice_id: row[0],
-          time_stamp: row[2],
-          object_changes: JSON.parse(row[3])
-        )
-      elsif row[1] == "Product"
-        Product.find_or_create_by(id:row[0])
-        ProductUpdate.create(
-          product_id: row[0],
-          time_stamp: row[2],
-          object_changes: JSON.parse(row[3])
-        )
+    if params[:file]
+      File.foreach(params[:file].path, headers:true) do |csv_line|
+        row = CSV.parse(csv_line.gsub('\"', '""')).first #For RubyCSV's inability to recognise escapes
+        if row[1] == "Order"
+          Order.find_or_create_by(id:row[0]) #Creates new order if non-existent
+          OrderUpdate.create(
+            order_id: row[0],
+            time_stamp: row[2],
+            object_changes: JSON.parse(row[3])
+          )
+        elsif row[1] == "Invoice"
+          Invoice.find_or_create_by(id:row[0])
+          InvoiceUpdate.create(
+            invoice_id: row[0],
+            time_stamp: row[2],
+            object_changes: JSON.parse(row[3])
+          )
+        elsif row[1] == "Product"
+          Product.find_or_create_by(id:row[0])
+          ProductUpdate.create(
+            product_id: row[0],
+            time_stamp: row[2],
+            object_changes: JSON.parse(row[3])
+          )
+        end
       end
     end
     redirect_to root_path
