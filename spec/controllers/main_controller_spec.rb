9a55e5ccefeedde0ddcf6a33b1c92df77a4ed13d
Justin Teo <justin.tsw@gmail.com>
2017-05-29 23:28:00 +0800

    Reconfigured Rspec to work for controller tests

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
+
+RSpec.describe MainController, type: :controller do
+
+  before :each do
+    file = fixture_file_upload('files/test.csv', 'text/csv')
+    post :import_csv, params: { file_upload: file }
+  end
+
   describe 'GET #index' do
     it "should render the index page" do
       get :index
@@ -7,10 +15,6 @@ require 'spec_helper'
   end
 
   describe "POST #import_csv" do
-    before :each do
-        file = fixture_file_upload('files/test.csv', 'text/csv')
-        post :import_csv, params: { file_upload: { csv: file } }
-    end
     it "should upload CSV and update corresponding database" do
       expect(Order.count).to eq(2)
       expect(Invoice.count).to eq(1)
@@ -19,17 +23,30 @@ require 'spec_helper'
       expect(InvoiceUpdate.count).to eq(2)
       expect(ProductUpdate.count).to eq(2)
     end
-    # it "Should redirect to root_path after upload" do
-        # response.should redirect_to '/'
-    # end
+
+    it "should redirect to root_path after upload" do
+      expect(response).to redirect_to('/')
+    end
   end
-  #
-  # describe "POST #search_state" do
-  #   before :each do
-  #     post :search_state
-  #   end
-  #   it "should return the correct object state" do
-  #
-  #   end
-  #
-  # end
+
+  describe "prepare_obj" do
+    it "should return the correct object state for order id 1 at time 1484733173" do
+      expect(controller.prepare_obj('Order',1,1484733173)).to eq({"customer_name"=>"Jack", "customer_address"=>"Trade St.", "status"=>"paid", "ship_date"=>"2017-01-18", "shipping_provider"=>"DHL"})
+    end
+
+    it "should return empty object for order id 1 at time 1484722542" do
+      expect(controller.prepare_obj('Order',1,1484722542)).to eq({})
+    end
+
+    it "should return correct object state for id 1 at time 1484731400" do
+      expect(controller.prepare_obj('Order',1,1484731400)).to eq({"customer_name"=>"Jack", "customer_address"=>"Trade St.", "status"=>"unpaid"})
+    end
+  end
+
+  describe "POST #search_state" do
+    it "should respond with javascript" do
+      post :search_state, format: :js
+      expect(response).to render_template("index")
+    end
+  end
+end
